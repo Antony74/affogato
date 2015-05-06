ebb599b97ebaba28f5e0af109939a9d86281b03a
Antony74 <akb@akb.me.uk>
2015-05-06 17:05:37 +0100

    Make the source code of a Processing sketch available.

--- library/vendors/affogato-markdown/affogato-markdown.php
+++ library/vendors/affogato-markdown/affogato-markdown.php
@@ -48,75 +48,98 @@ class AffogatoMarkdown_Parser extends MarkdownExtra_Parser
             }xm',
             function($matches)
             {
-                return CreateSketch($matches[2]);
+                return $this->CreateSketch($matches);
             },
             $text);
 
         return $text;
     }
 
+    //
+    // CreateSketch
+    //
+    // This is a bit awkward because we there are four levels of scripts:
+    //
+    // 1. PHP
+    // 2. JavaScript in the main window
+    // 3. JavaScript in the iframe
+    // 4. The code for the Processing sketch itself
+    //
+
+    function CreateSketch($matches)
+    {
+        $sID = uniqid();
+
+        $sProcessingCode = str_replace('"', "'", json_encode($matches[2], JSON_HEX_QUOT));
+        $nDocumentRootLength = count(explode(DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT'])) + 1;
+        $sRootUrl = '/' . implode(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, __FILE__), $nDocumentRootLength, -4));
+
+        return "<p>                                                                           \r\n"
+        .      "<div style='resize:both; overflow: hidden; border: 1px solid black'>          \r\n"
+        .      "    <iframe sandbox='allow-scripts' style='width:100%; height 100%;' scrolling='no' srcdoc=\"   \r\n"
+        .      "        <html>                                                                \r\n"
+        .      "        <head>                                                                \r\n"
+        .      "            <title>sketch</title>                                             \r\n"
+        .      "            <script src='{$sRootUrl}/js/library/jquery.js'></script>          \r\n"
+        .      "            <script src='{$sRootUrl}/library/vendors/affogato-markdown/processing.js'></script>\r\n"
+        .      "        </head>                                                               \r\n"
+        .      "        <body style='margin: 0px'>                                            \r\n"
+        .      "            <table id='play-{$sID}' style='width: 100%'>                      \r\n"
+        .      "                <tr style='height: 150px'>                                    \r\n"
+        .      "                    <td style='text-align:center'>                            \r\n"
+        .      "                        &#9654;                                               \r\n"
+        .      "                    </td>                                                     \r\n"
+        .      "                </tr>                                                         \r\n"
+        .      "            </table>                                                          \r\n"
+        .      "            <canvas id='{$sID}'>                                              \r\n"
+        .      "            </canvas>                                                         \r\n"
+        .      "            <script>                                                          \r\n"
+        .      "                $(document).ready(function()                                  \r\n"
+        .      "                {                                                             \r\n"
+        .      "                    if ('sandbox' in document.createElement('iframe'))        \r\n"
+        .      "                    {                                                         \r\n"
+        .      "                        $('body').one('click', function()                     \r\n"
+        .      "                        {                                                     \r\n"
+        .      "                            $('#play-{$sID}').remove();                       \r\n"
+        .      "                            new Processing('{$sID}', {$sProcessingCode});     \r\n"
+        .      "                        });                                                   \r\n"
+        .      "                    }                                                         \r\n"
+        .      "                    else                                                      \r\n"
+        .      "                    {                                                         \r\n"
+        .      "                        $('body').html('Sorry, your browser does not support iframe sandbox'); \r\n"
+        .      "                    }                                                         \r\n"
+        .      "               });                                                            \r\n"
+        .      "           </script>                                                          \r\n"
+        .      "        </body>                                                               \r\n"
+        .      "        </html>\">                                                            \r\n"
+        .      "        Sorry, your browser does not support iframes                          \r\n"
+        .      "    </iframe>                                                                 \r\n"
+        .      "</div>                                                                        \r\n"
+        .      "<A href='.' id='showCode{$sID}'>Show code</A>                                 \r\n"
+        .      "<div id='theCode{$sID}' style='position: relative; top: -10px'>               \r\n"
+        .      $this->_doFencedCodeBlocks_callback($matches)
+        .      "</div>                                                                        \r\n"
+        .      "<script>                                                                      \r\n"
+        .      "    $(document).ready(function()                                              \r\n"
+        .      "    {                                                                         \r\n"
+        .      "        $('#theCode{$sID}').hide();                                           \r\n"
+        .      "        $('#showCode{$sID}').toggle(function()                                \r\n"
+        .      "        {                                                                     \r\n"
+        .      "            $('#theCode{$sID}').show();                                       \r\n"
+        .      "            $('#showCode{$sID}').html('Hide code');                           \r\n"
+        .      "            return false;                                                     \r\n"
+        .      "        }, function()                                                         \r\n"
+        .      "        {                                                                     \r\n"
+        .      "            $('#theCode{$sID}').hide();                                       \r\n"
+        .      "            $('#showCode{$sID}').html('Show code');                           \r\n"
+        .      "            return false;                                                     \r\n"
+        .      "        });                                                                   \r\n"
+        .      "    });                                                                       \r\n"
+        .      "</script>                                                                     \r\n"
+        .      "</p>                                                                          \r\n";
+    }
+
 };
-
-//
-// CreateSketch
-//
-// This is a bit awkward because we there are four levels of scripts:
-//
-// 1. PHP
-// 2. JavaScript in the main window
-// 3. JavaScript in the iframe
-// 4. The code for the Processing sketch itself
-//
-
-function CreateSketch($sRawProcessingCode)
-{
-    $sID = uniqid();
-
-    $sProcessingCode = str_replace('"', "'", json_encode($sRawProcessingCode, JSON_HEX_QUOT));
-    $nDocumentRootLength = count(explode(DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT'])) + 1;
-    $sRootUrl = '/' . implode(DIRECTORY_SEPARATOR, array_slice(explode(DIRECTORY_SEPARATOR, __FILE__), $nDocumentRootLength, -4));
-
-    return "<p><div style='resize:both; overflow: hidden; border: 1px solid black'>       \r\n"
-    .      "    <iframe sandbox='allow-scripts' style='width:100%; height 100%;' scrolling='no' srcdoc=\"   \r\n"
-    .      "        <html>                                                                \r\n"
-    .      "        <head>                                                                \r\n"
-    .      "            <title>sketch</title>                                             \r\n"
-    .      "            <script src='{$sRootUrl}/js/library/jquery.js'></script>          \r\n"
-    .      "            <script src='{$sRootUrl}/library/vendors/affogato-markdown/processing.js'></script>\r\n"
-    .      "        </head>                                                               \r\n"
-    .      "        <body style='margin: 0px'>                                            \r\n"
-    .      "            <table id='play-{$sID}' style='width: 100%'>                      \r\n"
-    .      "                <tr style='height: 150px'>                                    \r\n"
-    .      "                    <td style='text-align:center'>                            \r\n"
-    .      "                        &#9654;                                               \r\n"
-    .      "                    </td>                                                     \r\n"
-    .      "                </tr>                                                         \r\n"
-    .      "            </table>                                                          \r\n"
-    .      "            <canvas id='{$sID}'>                                              \r\n"
-    .      "            </canvas>                                                         \r\n"
-    .      "            <script>                                                          \r\n"
-    .      "                $(document).ready(function()                                  \r\n"
-    .      "                {                                                             \r\n"
-    .      "                    if ('sandbox' in document.createElement('iframe'))        \r\n"
-    .      "                    {                                                         \r\n"
-    .      "                        $('body').one('click', function()                     \r\n"
-    .      "                        {                                                     \r\n"
-    .      "                            $('#play-{$sID}').remove();                       \r\n"
-    .      "                            new Processing('{$sID}', {$sProcessingCode});     \r\n"
-    .      "                        });                                                   \r\n"
-    .      "                    }                                                         \r\n"
-    .      "                    else                                                      \r\n"
-    .      "                    {                                                         \r\n"
-    .      "                        $('body').html('Sorry, your browser does not support iframe sandbox'); \r\n"
-    .      "                    }                                                         \r\n"
-    .      "               });                                                            \r\n"
-    .      "           </script>                                                          \r\n"
-    .      "        </body>                                                               \r\n"
-    .      "        </html>\">                                                            \r\n"
-    .      "        Sorry, your browser does not support iframes                          \r\n"
-    .      "    </iframe>                                                                 \r\n"
-    .      "</div></p>                                                                    \r\n";
-}
 
 ?>
 
